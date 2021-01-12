a87bb7ee7c0aa53aaf5c5b99d84a4b8a3b236bb0
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-01-12 21:15:01 +0100

    layout and styling of page
    plus a little animation here and there :wink:

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('./public'));
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.listen(process.env.PORT || port, () => {
     console.log(`Express app running at http://localhost:${port}`);
 });
